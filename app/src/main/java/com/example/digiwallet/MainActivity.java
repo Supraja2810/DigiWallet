ebb99e99ce42f554dfd17cd1b0dfa7f0459c9f87
Supraja2810 <suprajabalu28@gmail.com>
2021-06-06 19:45:57 +0530

    Commit

--- app/src/main/java/com/example/digiwallet/MainActivity.java
+++ app/src/main/java/com/example/digiwallet/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.digiwallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CardView card1,card2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        card1 = (CardView)findViewById(R.id.card1);
+        card2 = (CardView)findViewById(R.id.card2);
 
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this,AddPlace.class);
+            startActivity(i);
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this,DisplayFriend.class);
+            startActivity(i);
+            }
+        });
     }
 }
