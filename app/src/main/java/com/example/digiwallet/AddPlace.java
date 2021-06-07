642061c06e8f4800a82a492b4693b110af1f1540
Supraja2810 <suprajabalu28@gmail.com>
2021-06-07 20:21:09 +0530

    Commit

--- app/src/main/java/com/example/digiwallet/AddPlace.java
+++ app/src/main/java/com/example/digiwallet/AddPlace.java
@@ -2,13 +2,25 @@ package com.example.digiwallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddPlace extends AppCompatActivity {
-
+    private FloatingActionButton floatingActionButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
+        floatingActionButton = (FloatingActionButton)findViewById(R.id.newbtn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AddPlace.this,AddPlaceDetails.class));
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/digiwallet/AddPlaceDetails.java
@@ -0,0 +1,14 @@
+package com.example.digiwallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPlaceDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_place_details);
+    }
+}
